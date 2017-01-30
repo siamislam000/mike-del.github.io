f95be245dc4131a03a27ec1c1717e2ae6e6d6ff8
Michael Delgado <intrepidcreativity@gmail.com>
2017-01-30 12:31:46 -0800

    changed wording on email link'

--- js/email_contact_button.js
+++ js/email_contact_button.js
@@ -5,11 +5,11 @@ $('.email_link').click(function(e) {
     e.preventDefault();
     // handle the form submission (AJAX...)
 
-    $(".email_link").html( "delgado.portland@gmail.com" );
+    $(".email_link").html( "Michael:  delgado.portland@gmail.com" );
 
     window.location.assign('mailto: delgado.portland@gmail.com');
 
-    $('body').mousedown(function() {
-	  $(".email_link").html( "Michael delgado.portland@gmail.com" );
-	}); 
+ //    $('body').mousedown(function() {
+	//   $(".email_link").html( "Michael delgado.portland@gmail.com" );
+	// }); 
 });
